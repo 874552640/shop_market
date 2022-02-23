647fc938b179e5ed248737d8f5499fd645a443bd
ycc <874552640@qq.com>
2022-02-23 14:36:49 +0800

    选择项目依赖

---
+++ market/src/test/java/com/ycc/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycc.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
